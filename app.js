bc1806a5799721340c42a19bc7b113920dc5e1ea
bonobo <arburpee@gmail.com>
2021-12-08 23:17:38 -0800

    modularized code and created html page

---
+++ app.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
+
+fs.writeFile("./index.html", generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('portfolio complete! check out index.html to see the output')
+})
+
+
