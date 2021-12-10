820991f6d2d8dffd212970300f6a067a092f7ede
bonobo <arburpee@gmail.com>
2021-12-09 23:40:53 -0800

    updated html template with filter and map

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-// const fs = require("fs");
-// const generatePage = require("./src/page-template.js");
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 // const pageHTML = generatePage(name, github);
 
 // fs.writeFile("./index.html", generatePage(name, github), err => {
@@ -142,6 +142,12 @@ const promptProject = portfolioData => {
 )};
 promptUser()
 .then(promptProject)
-.then(portfolioData=> {
-console.log(portfolioData);
-})
+.then(portfolioData => {
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+        if (err) throw new Error(err);
+    console.log('page created! check out index.html in this directory')
+
+    });
+});
