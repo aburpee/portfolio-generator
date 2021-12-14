7de327a739df88d895255badd666f700b3b1e590
bonobo <arburpee@gmail.com>
2021-12-13 20:00:43 -0800

    added css properties and refactored

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 const inquirer = require('inquirer');
-const fs = require("fs");
 const generatePage = require("./src/page-template.js");
-// const pageHTML = generatePage(name, github);
+const { writeFile, copyFile } = require("./utils/generate-site.js");
 
-// fs.writeFile("./index.html", generatePage(name, github), err => {
-//     if (err) throw new Error(err);
-
-//     console.log('portfolio complete! check out index.html to see the output')
-// })
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -143,11 +137,18 @@ const promptProject = portfolioData => {
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./index.html', pageHTML, err => {
-        if (err) throw new Error(err);
-    console.log('page created! check out index.html in this directory')
-
-    });
+return generatePage(portfolioData);
+})
+.then(pageHTML => {
+    return writeFile(pageHTML);
+})
+.then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+})
+.then(copyFileResponse => {
+    console.log(copyFileResponse);
+})
+.catch(err => {
+    console.log(err);
 });
